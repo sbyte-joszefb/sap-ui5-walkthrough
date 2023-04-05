cdcfc3c8b9c8c7ab5f2f45c8ce51133b7d4c6463
sbyte-joszefb <joszef.sinosbyte@gmail.com>
2023-04-05 11:41:58 -0300

    Passo 5 - Controllers

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function(Controller){
+    "use strict"
+    return Controller.extend(
+        "sap.ui.demo.walkthrough.App",
+        {
+            onShowHello : function () {
+                // Show a native or vanailla JS alert
+                alert("Hello there!");
+                alert("General Progromer! You a bold one!");
+            }
+        }
+    );
+    
+});
