8ee804aabcc1b3d45c9d4724ec9d119a02f06b09
sbyte-joszefb <joszef.sinosbyte@gmail.com>
2023-04-19 11:46:55 -0300

    Passo 15 - Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,13 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast"
+        "sap/ui/core/mvc/Controller"
     ],
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict"
 
         return Controller.extend(
             "sap.ui.demo.walkthrough.App",
-            {
-                onShowHello: function () {
-                    // read msg from i18n model
-                    var oBundle = this.getView().getModel("i18n").getResourceBundle();
-                    var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-                    var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-                    // Show message
-                    MessageToast.show(sMsg);
-                }
-            }
+            {}
         );
     }
 );
