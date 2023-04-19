14aac65403313863330afe7ad76778581c530534
sbyte-joszefb <joszef.sinosbyte@gmail.com>
2023-04-19 12:01:00 -0300

    Passo 16 - Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast"
+        "sap/m/MessageToast",
+        "sap/ui/core/Fragment"
     ],
-    function (Controller, MessageToast) {
+    function (Controller, MessageToast, fragment) {
         'use strict';
         return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel",
             {
@@ -14,6 +15,25 @@ sap.ui.define(
                     var sMsg = oBundle.getText("helloMsg", [sRecipient]);
                     // Show message
                     MessageToast.show(sMsg);
+                },
+                onopendialog: function() {
+                    var oView = this.getView();
+
+                    // create the dialog lazily
+                    if (!this.byId("helloDialog")) {
+                    // load asynchronous XML fragment
+                    Fragement.load({
+                        id: oView.getId(),
+                        name: "sap.ui.demo.walkthrough.view.HelloDialog"
+                    }).then(function (oDialog) {
+                        // connect dialog to the root view of this component (models, lifecycle)
+                        oView.addDependent(oDialog);
+                        oDialog.open();
+                    })
+                    }
+                    else {
+                        this.byId("helloDialog").open();
+                    }
                 }
             }
         )
