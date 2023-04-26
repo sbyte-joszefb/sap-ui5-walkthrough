efc268b56b9a1d29b7eb63fe5185a55cf4f906da
sbyte-joszefb <joszef.sinosbyte@gmail.com>
2023-04-26 11:29:38 -0300

    Passo 24 - Filtering

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -2,9 +2,11 @@ sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
         "sap/ui/model/json/JSONModel",
-        "../model/formatter"
+        "../model/formatter",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator"
     ],
-    function (Controller, JSONModel, formatter) {
+    function (Controller, JSONModel, formatter, Filter, FilterOperator) {
         "use strict"
     
         return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList",
@@ -17,6 +19,20 @@ sap.ui.define(
                         }
                     );
                     this.getView().setModel(oViewModel, "view")
+                },
+                onFilterInvoices : function (oEvent) {
+        
+                    // build filter array
+                    var aFilter = [];
+                    var sQuery = oEvent.getParameter("query");
+                    if (sQuery) {
+                        aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+                    }
+        
+                    // filter binding
+                    var oList = this.byId("invoiceList");
+                    var oBinding = oList.getBinding("items");
+                    oBinding.filter(aFilter);
                 }
             }
         )
