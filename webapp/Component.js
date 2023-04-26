d5957512cccb339248234214e347d497003f3aa2
sbyte-joszefb <joszef.sinosbyte@gmail.com>
2023-04-26 10:50:30 -0300

    Passo 19 - Reuse Dialogs

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,32 +1,42 @@
 sap.ui.define(
-    [
-        "sap/ui/core/UIComponent",
-        "sap/ui/model/json/JSONModel",
-        "sap/ui/model/resource/ResourceModel"
-    ],
-    function (UIComponent, JSONModel, ResourceModel){
-        "use strict";
+	[
+		"sap/ui/core/UIComponent",
+		"sap/ui/model/json/JSONModel",
+		"sap/ui/model/resource/ResourceModel",
+		"./controller/HelloDialog"
+	],
+	function (UIComponent, JSONModel, ResourceModel, HelloDialog) {
+		"use strict";
+		return UIComponent.extend("sap.ui.demo.walkthrough.Component",
+			{
+				metadata: {
+					manifest: "json"
+				},
+				init: function () {
+					// call the init function of the parent
+					UIComponent.prototype.init.apply(this, arguments);
+					// set data models
+					var oData = {
+						recipient: {
+							name: "UI5"
+						}
+					};
+					var oModel = new JSONModel(oData);
+					this.setModel(oModel);
 
-        return UIComponent.extend(
-            "sap.ui.demo.walkthrough.Component",
-            {
-                metadata : {
-                    manifest: "json"
-                },
-                
-                init : function () {
-                    // call the init function of the parent
-                    UIComponent.prototype.init.apply(this, arguments);
-                    // set data models
-                    var oData = {
-                        recipient: {
-                            name: "UI5"
-                        }
-                    };
-                    var oModel = new JSONModel(oData);
-                    this.setModel(oModel);
-                }
-            }
-        );
-    }
+					// set dialog
+					this._helloDialog = new HelloDialog(this.getRootControl());
+				},
+
+				exit: function () {
+					this._helloDialog.destroy();
+					delete this._helloDialog;
+				},
+
+				openHelloDialog : function () {
+					this._helloDialog.open();
+				} 
+			}
+		);
+	}
 );
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,13 +1,17 @@
 sap.ui.define(
-    [
-        "sap/ui/core/mvc/Controller"
-    ],
-    function (Controller) {
-        "use strict"
+	[
+		"sap/ui/core/mvc/Controller"
+	],
+	function (Controller) {
+		"use strict"
+		return Controller.extend("sap.ui.demo.walkthrough.App",
+			{
+				
+				onOpenDialog: function () {
+					this.getOwnerComponent().openHelloDialog();
+				}
 
-        return Controller.extend(
-            "sap.ui.demo.walkthrough.App",
-            {}
-        );
-    }
+			}
+		);
+	}
 );
